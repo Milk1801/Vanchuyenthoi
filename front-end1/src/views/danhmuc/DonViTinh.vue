2b7a94f30a30505aeb3dc8448df5c3be2ef78dce
Hai <Hai95546@st.vimaru.edu.vn>
2026-04-03 17:18:58 +0700

    thêm giao diện cơ bản cho các loại danh mục

---
+++ front-end1/src/views/danhmuc/DonViTinh.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h4>Danh mục Đơn vị tính</h4>
+    <p>Danh sách đơn vị tính: chiếc, tấn, mét, lít, ...</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ front-end1/src/views/danhmuc/HangHoa.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h4>Danh mục Hàng hóa</h4>
+    <p>Quản lý danh mục hàng hóa: phân loại, mô tả, đơn giá.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
